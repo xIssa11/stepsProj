a70bdbe1b8f4a6cdcef47366d26eac069777cd47
xIssa11 <issahashim@yahoo.com>
2024-03-18 14:55:58 -0700

    Created StepMessage Function

--- src/App.js
+++ src/App.js
@@ -41,9 +41,9 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}> {messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button
               bgColor={"#7950f2"}
@@ -59,6 +59,14 @@ function Steps() {
         </div>
       )}
     </div>
+  );
+}
+function StepMessage({ step, children }) {
+  return (
+    <p className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </p>
   );
 }
 
